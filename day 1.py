118a49ff7e32eb4ae726c6fb6141be38da9b7254
abdullahalrashed <raashare1@gmail.com>
2026-04-02 22:05:53 +0600

    day 1

---
+++ day 1.py
@@ -0,0 +1,13 @@
+print("hello world")
+print("hello" + input("what is your name? " + "!"))
+
+print(len(input("what is your name? ")))
+
+username = input("what is your name? ")
+print(username)
+print(len(username))
+
+print("Welcome to the Band Name Generator.")
+city_name = input("Whats the name of the city you grew up in?\n")
+pets_name = input("Whats your pet's name?\n")
+print("Your band name could be " + city_name + " " + pets_name)
