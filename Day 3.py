581215fc35b7c168fd238c92764019e1568c0ceb
abdullahalrashed <raashare1@gmail.com>
2026-04-04 11:11:37 +0600

    day 3.7

--- Day 3.py
+++ Day 3.py
@@ -37,8 +37,8 @@ height = 1.85
 
 bmi = weight / (height ** 2)
 
-# 🚨 Do not modify the values above
-# Write your code below 👇
+# Do not modify the values above
+# Write your code below
 
 if bmi <18.5:
     print("underweight")
