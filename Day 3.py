4a2c3d0cdc1043e564fe7b73f4293f3f26771e81
abdullahalrashed <raashare1@gmail.com>
2026-04-04 09:20:42 +0600

    day 3.2

--- Day 3.py
+++ Day 3.py
@@ -1,7 +1,16 @@
 print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+height = int(input("What is your height in cm?\n"))
 
-if height > 120:
- print("You can ride the rollercoaster")
+if height >= 120:
+    print("You can ride the rollercoaster")
 else:
- print("Sorry you have to grow taller before you can ride.")
+    print("Sorry you have to grow taller before you can ride.")
+
+##Even odd
+
+number_to_check = int(input("What is the number you want to check?\n"))
+
+if number_to_check % 2 == 0:
+    print ("Even")
+else:
+    print("0dd")
