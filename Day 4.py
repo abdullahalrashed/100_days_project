9af39a4807e2c7313f5b319f698f77eb98b0529e
abdullahalrashed <raashare1@gmail.com>
2026-04-06 14:10:50 +0600

    day 4.6

--- Day 4.py
+++ Day 4.py
@@ -1,39 +1,107 @@
 import random
 
-random_integer = random.randint(1, 10)
-print(random_integer)
+#
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+#
+# random_number_0_to_1 = random.random() * 10
+# print(random_number_0_to_1)
+#
+# random_float = random.uniform(1, 10)
+# print(random_float)
+#
+# random_heads_or_tails = random.randint(0, 1)
+# if random_heads_or_tails == 0:
+#     print("Heads")
+# else:
+#     print("Tails")
+#
+# states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia"]
+#
+# states_of_america[1] = "Pengilvania"
+#
+# states_of_america.append("Angelaland")
+# states_of_america.extend(["Angelaland2", "Jack Bauer Land"])
+# print(states_of_america)
+#
+# number = random.randint(0, 4)
+# friends = ["alice", "Bob", "Charlie", "David", "Emanuel"]
+# name = friends[number]
+# print(name)
+#
+# friends2 = ["alice", "Bob", "Charlie", "David", "Emanuel"]
+# name2 = random.choice(friends2)
+# print(name2)
+#
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries"]
+# vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+#
+# dirty_dozen = [fruits, vegetables]
+# print(dirty_dozen[0][0])
 
-random_number_0_to_1 = random.random() * 10
-print(random_number_0_to_1)
+# rock paper scissors game
 
-random_float = random.uniform(1, 10)
-print(random_float)
+rock = (''' 
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
 
-random_heads_or_tails = random.randint(0, 1)
-if random_heads_or_tails == 0:
-    print("Heads")
+''')
+
+paper = ('''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+
+''')
+
+scissors = ('''
+
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+''')
+
+art = [rock, paper, scissors]
+names = ["rock", "paper", "scissors"]
+computer_choice = random.choice(names)
+user_choice = input("Choose between Rock, Paper, or Scissors:\n").lower()
+
+if computer_choice == user_choice:
+    print(f"Computer chose {computer_choice} and you chose {user_choice}")
+    print("It's a tie!")
+elif computer_choice == "rock" and user_choice == "paper":
+    print(f"Computer chose {computer_choice} and you chose {user_choice}")
+    print(rock + paper)
+    print("You win!")
+elif computer_choice == "rock" and user_choice == "scissors":
+    print(f"Computer chose {computer_choice} and you chose {user_choice}")
+    print(rock + scissors)
+    print(" computer Wins")
+elif computer_choice == "paper" and user_choice == "rock":
+    print(f"Computer chose {computer_choice} and you chose {user_choice}")
+    print(paper + rock)
+    print("Computer Wins!")
+elif computer_choice == "paper" and user_choice == "scissors":
+    print(f"Computer chose {computer_choice} and you chose {user_choice}")
+    print(paper + scissors)
+    print("You win!")
+elif computer_choice == "scissors" and user_choice == "paper":
+    print(f"Computer chose {computer_choice} and you chose {user_choice}")
+    print(scissors + paper)
+    print("Computer Wins!")
+elif computer_choice == "scissors" and user_choice == "rock":
+    print(f"Computer chose {computer_choice} and you chose {user_choice}")
+    print(scissors + rock)
+    print("You win!")
 else:
-    print("Tails")
-
-states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia"]
-
-states_of_america[1] = "Pengilvania"
-
-states_of_america.append("Angelaland")
-states_of_america.extend(["Angelaland2", "Jack Bauer Land"])
-print(states_of_america)
-
-number = random.randint(0, 4)
-friends = ["alice", "Bob", "Charlie", "David", "Emanuel"]
-name = friends[number]
-print(name)
-
-friends2 = ["alice", "Bob", "Charlie", "David", "Emanuel"]
-name2 = random.choice(friends2)
-print(name2)
-
-fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries"]
-vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
-
-dirty_dozen = [fruits, vegetables]
-print(dirty_dozen)
+    print("You chose a wrong choice. Try again.")
